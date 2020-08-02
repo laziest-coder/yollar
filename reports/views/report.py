464d246703308097878eb8a765738a9c3c09c33c
laziestCoder <jasurbekyoldoshev98@gmail.com>
2020-08-02 16:04:03 +0500

    add permission classes to report view and fix root urls

--- reports/views/report.py
+++ reports/views/report.py
@@ -1,11 +1,16 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework import permissions
 from reports.models import Report, Image
+from reports.serializers import ReportSerializer
 
 
 class ReportList(APIView):
 
+    permission_classes = [permissions.IsAuthenticated]
+
     def get(self, request):
         reports = Report.objects.all()
-        return Response(reports)
+        serializer = ReportSerializer(reports, many=True)
+        return Response(serializer.data)
--- yollar/urls.py
+++ yollar/urls.py
@@ -1,13 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from profiles import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
-
-urlpatterns += [
     path('api/profile/register', views.CustomAuthToken.as_view()),
     path('api/profile/', views.UserProfileApiView.as_view()),
     path('api/reports/', include('reports.urls'))
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
